bda76131cf84f699a415690f6966dcaaedfcb7aa
Davide Brunato <brunato@sissa.it>
2020-11-16 09:33:36 +0100

    Add performance comparison tests for XPath processor (elementpath)

--- README.rst
+++ README.rst
@@ -3,8 +3,12 @@ xmlschema-benchmarks
 ********************
 
 A repository with a set of test scripts for comparing
-`xmlschema <https://github.com/brunato/xmlschema>`_ performance
+`xmlschema <https://github.com/sissaschool/xmlschema>`_ performance
 against to `lxml <https://github.com/lxml/lxml>`_'s *XMLSchema* validator.
+XPath selection comparison is operated between
+`elementpath <https://github.com/sissaschool/elementpath>`_,
+Python's standard library XPath selectors and lxml.
+
 The XSD/XML files used for comparison are from
 `OneLogin's SAML Python Toolkit repository <https://github.com/onelogin/python3-saml>`_.
 
@@ -12,6 +16,8 @@ Available performance tests:
 
 * *timing_schema_build.py* : performance on building of the SAML2 schemas
 * *timing_validation.py* : performance on validation of SAML2 XML data
+* *timing_xpath.py* : performance of XPath selection on SAML2 XML data
+
 
 Results
 =======
@@ -20,11 +26,18 @@ Performance comparison:
 
 * Schemas build is ~70-80 times faster with lxml, only ~5 times with loading
   of serialized schemas with pickle;
-* Validation is ~50 times faster with lxml
+* Validation is ~63-77 times faster with lxml, but lxml validates an XML file
+  that has 2 invalid base64 values;
+* XPath selection is ~20-30 times faster with lxml and is ~60-85 times faster
+  with ElementTree
 
-Even lxml is more faster with these types of schemas, xmlschema cover full XSD 1.0
+Even lxml is more faster with these types of schemas, xmlschema covers full XSD 1.0
 and XSD 1.1 and has other protection features over malicious constructed XSD/XML
 sources (eg. MAX_XML_DEPTH/MAX_MODEL_DEPTH).
+
+For XPath the difference against lxml is reduced, and lxml has only selectors for
+XPath 1.0. The difference against the XPath selectors of the standard library is
+higher but ElementTree doesn't have a full XPath processor.
 
 Future improvements could be tentatives of using type-hints and optional
 Cythonized modules.
---
+++ timing_xpath.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+#
+# BDS 3-clause licence, copyright 2020, SISSA
+# @Author: Davide Brunato
+#
+"""
+Performance comparison of XPath selection on SAML2 XML data with elementpath,
+xml.etree.ElementPath and lxml.
+"""
+import os
+from timeit import timeit
+from xml.etree import ElementTree
+import lxml.etree as etree
+from elementpath import select, XPath1Parser
+
+
+PROJECT_DIR = os.path.dirname(__file__)
+
+REQUEST_XML_FILE = os.path.join(PROJECT_DIR, 'data/authn_request.xml')
+RESPONSE_XML_FILE = os.path.join(PROJECT_DIR, 'data/simple_saml_php.xml')
+METADATA_XML_FILE = os.path.join(PROJECT_DIR, 'data/testshib-providers.xml')
+
+
+def run_timeit(stmt='pass', setup='pass', number=10):
+    print("{}: {}s".format(stmt, timeit(stmt, setup=setup, number=number)))
+
+
+request_root = ElementTree.parse(REQUEST_XML_FILE).getroot()
+response_root = ElementTree.parse(RESPONSE_XML_FILE).getroot()
+metadata_root = ElementTree.parse(METADATA_XML_FILE).getroot()
+
+request_lxml_root = etree.parse(REQUEST_XML_FILE).getroot()
+response_lxml_root = etree.parse(RESPONSE_XML_FILE).getroot()
+metadata_lxml_root = etree.parse(METADATA_XML_FILE).getroot()
+
+value = 'http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p'
+namespaces = {'md': 'urn:oasis:names:tc:SAML:2.0:metadata'}
+
+path1 = "/md:EntitiesDescriptor/md:EntityDescriptor/md:SPSSODescriptor/" \
+        "md:KeyDescriptor/md:EncryptionMethod[@Algorithm={!r}]".format(value)
+
+XPath1Parser.build()
+
+print("### Performance test of XPath selection over SAML2 XML data ###\n")
+
+
+def elementpath_path_with_predicate():
+    results = select(metadata_root, path1, namespaces, parser=XPath1Parser)
+    assert len(results) == 1
+    assert results[0] is metadata_root[1][1][1][9]
+    assert results[0].get('Algorithm') == value
+
+
+def element_tree_path_with_predicate():
+    results = metadata_root.findall(path1[23:], namespaces=namespaces)
+    assert len(results) == 1
+    assert results[0] is metadata_root[1][1][1][9]
+    assert results[0].get('Algorithm') == value
+
+
+def lxml_etree_path_with_predicate():
+    results = metadata_lxml_root.xpath(path1, namespaces=namespaces)
+    assert len(results) == 1
+    assert results[0] is metadata_lxml_root[5][2][1][9]
+    assert results[0].get('Algorithm') == value
+
+
+def elementpath_lxml_path_with_predicate():
+    results = select(metadata_lxml_root, path1, namespaces, parser=XPath1Parser)
+    assert len(results) == 1
+    assert results[0] is metadata_lxml_root[5][2][1][9]
+    assert results[0].get('Algorithm') == value
+
+
+def elementpath_descendants():
+    results = select(response_root, './/*', parser=XPath1Parser)
+    assert len(results) == 46
+
+
+def element_tree_descendants():
+    results = response_root.findall('.//*')
+    assert len(results) == 46
+
+
+def lxml_etree_descendants():
+    results = response_lxml_root.xpath('.//*')
+    assert len(results) == 46
+
+
+def elementpath_lxml_descendants():
+    results = select(response_lxml_root, './/*', parser=XPath1Parser)
+    assert len(results) == 46
+
+
+NUMBER = 300
+
+SETUP = 'from __main__ import ' \
+        'elementpath_path_with_predicate, ' \
+        'element_tree_path_with_predicate, ' \
+        'lxml_etree_path_with_predicate, ' \
+        'elementpath_lxml_path_with_predicate, ' \
+        'elementpath_descendants, ' \
+        'element_tree_descendants, ' \
+        'lxml_etree_descendants, ' \
+        'elementpath_lxml_descendants'
+
+
+run_timeit("elementpath_path_with_predicate()", setup=SETUP, number=NUMBER)
+run_timeit("element_tree_path_with_predicate()", setup=SETUP, number=NUMBER)
+run_timeit("lxml_etree_path_with_predicate()", setup=SETUP, number=NUMBER)
+run_timeit("elementpath_lxml_path_with_predicate()", setup=SETUP, number=NUMBER)
+
+print()
+
+run_timeit("elementpath_descendants()", setup=SETUP, number=NUMBER)
+run_timeit("element_tree_descendants()", setup=SETUP, number=NUMBER)
+run_timeit("lxml_etree_descendants()", setup=SETUP, number=NUMBER)
+run_timeit("elementpath_lxml_descendants()", setup=SETUP, number=NUMBER)
